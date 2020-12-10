1844fc55da7534233d2cfbce085eef0b063e23cd
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-09 19:54:16 -0800

    test set up preparing for model

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -2,9 +2,25 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app endpoint', () => {
-    it('creates a new car via post', async() => {
-        const res = await request(app).get('/');
-        
-        expect(res.body).toEqual({ hello: 'world' });
+    it('creates a new car by post', async() => {
+        const res = await request(app)
+        .post('/api/v1/tesla/model_s');
+        .send({
+            title: 'model s',
+            descript: 'very nice',
+            model: 'extended',
+            color: 'black',
+            wheelType: 'ion',
+            interior: 'black'
+        });
+
+        expect(res.body).toEqual({ 
+            title: 'model s',
+            descript: 'very nice',
+            model: 'extended',
+            color: 'black',
+            wheelType: 'ion',
+            interior: 'black'
+        });
     }); 
 });
