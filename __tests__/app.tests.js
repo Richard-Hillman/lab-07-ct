2810ca323cca089713a92df2876cc2b0b4d3ca5e
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-09 20:04:51 -0800

    insert method created setup sql file created pool and fs imprted to test

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -1,26 +1,30 @@
+const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+
+// -------------------------------------------
 
 describe('app endpoint', () => {
-    it('creates a new car by post', async() => {
-        const res = await request(app)
-        .post('/api/v1/tesla/model_s');
-        .send({
-            title: 'model s',
-            descript: 'very nice',
-            model: 'extended',
-            color: 'black',
-            wheelType: 'ion',
-            interior: 'black'
-        });
+  it('creates a new car by post', async() => {
+    const res = await request(app)
+      .post('/api/v1/tesla/model_s')
+      .send({
+        title: 'model s',
+        descript: 'very nice',
+        model: 'extended',
+        color: 'black',
+        wheelType: 'ion',
+        interior: 'black'
+      });
 
-        expect(res.body).toEqual({ 
-            title: 'model s',
-            descript: 'very nice',
-            model: 'extended',
-            color: 'black',
-            wheelType: 'ion',
-            interior: 'black'
-        });
-    }); 
+    expect(res.body).toEqual({ 
+      title: 'model s',
+      descript: 'very nice',
+      model: 'extended',
+      color: 'black',
+      wheelType: 'ion',
+      interior: 'black'
+    });
+  }); 
 });
---
+++ sql/setup.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS model_s
+
+ CREATE TABLE  model_s (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  descript TEXT, 
+  model TEXT,
+  color TEXT,
+  wheel_type TEXT,
+  interior TEXT
+);
